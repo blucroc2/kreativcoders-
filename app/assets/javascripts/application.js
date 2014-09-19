9a0c5741ddb461928cb8d38ee5ee340b9738e990
akhil <akhilinror@gmail.com>
2014-09-19 15:26:03 +0530

    kreative coders site

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,30 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+
+//= require_tree .
+//= require modernizr-2.6.2-respond-1.1.0.min.js
+//* require jquery-1.11.1.min.js
+//* require jquery-1.11.1.min.js
+//* require bootstrap.min.js
+//* require owl.carousel.min.js
+//* require jquery.sticky.js 
+//* require masonry.pkgd.min.js
+//* require jquery.waitforimages.js
+//* require jquery.carouFredSel-6.2.1-packed.js
+//* require jquery.easing-1.3.pack.js
+//* require jquery.vegas.min.js
+//* require parallax.js
+//* require main.js
+
